d3e5633c878e19ad2866c1bc24d44108d2fa90ab
Penprapa Calhoun <mayyeewa@yahoo.com>
2021-04-11 11:59:15 -0400

    first commit

---
+++ src/week1/KarelDefendsDemocracy.java
@@ -0,0 +1,50 @@
+package week1;
+import stanford.karel.*;
+
+public class KarelDefendsDemocracy extends SuperKarel {
+
+	public void run() {
+		while (frontIsClear() ) {
+			move();
+			checkMiddleBeeper();
+			cleanExtraBeeper();
+			move();
+			
+		}				
+	}
+	private void checkMiddleBeeper()  {
+		if (beepersPresent()) {
+			move();
+			move();
+			
+		}
+	}
+	private void cleanExtraBeeper() {
+		if (noBeepersPresent() ) {
+			cleanBeepersSouth();
+			cleanBeepersNorth();
+		}
+	}
+	private void cleanBeepersSouth() {
+		turnRight();
+		move();
+		while (beepersPresent()) {
+			pickBeeper();
+		}
+		turnAround();
+		move();
+	}
+	private void cleanBeepersNorth() {
+		move ();
+		if (frontIsClear()) {
+			move();
+		}
+		while (beepersPresent() ) {
+			pickBeeper();
+			
+		}
+		turnAround();
+		move();
+		turnLeft();
+		}
+	}
