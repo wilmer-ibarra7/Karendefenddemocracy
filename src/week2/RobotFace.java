5f570bc0e59c104170fd4a972ac3efe9a76565a4
Penprapa Calhoun <mayyeewa@yahoo.com>
2021-04-16 14:08:48 -0400

    first commit

--- src/week2/RobotFace.java
+++ src/week2/RobotFace.java
@@ -1,7 +1,65 @@
 package week2;
 
+import java.awt.Color;
+
+import acm.graphics.G3DRect;
+import acm.graphics.GOval;
+import acm.graphics.GRectangle;
 import acm.program.GraphicsProgram;
 
+@SuppressWarnings("unused")
 public class RobotFace extends GraphicsProgram {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/* Parameters for the drawing */
+		private static final int HEAD_WIDTH = 100;
+		private static final int HEAD_HEIGHT = 150;
+		private static final int EYE_RADIUS = 10;
+		private static final int MOUTH_WIDTH = 60;
+		private static final int MOUTH_HEIGHT = 20;
+		
+	public void run() {
+		addFace (getWidth() / 2, getHeight() / 2);
+	}
+	
+	/* Adds the entire face centered at (cx, cy) */
+	private void addFace(double cx, double cy) {
+		addHead(cx, cy);
+		addEye(cx - HEAD_WIDTH / 4, cy - HEAD_HEIGHT / 4);
+		addEye(cx + HEAD_WIDTH /4, cy - HEAD_HEIGHT / 4);
+		addMouth (cx, cy + HEAD_HEIGHT / 4);
+	}
 
+	/* Adds the head centered at (cx, cy) */
+	private void addHead (double cx, double cy) {
+		double x = cx - HEAD_WIDTH / 2;
+		double y = cy - HEAD_HEIGHT / 2;
+		G3DRect head = new G3DRect (x, y, HEAD_WIDTH, HEAD_HEIGHT) ;
+		head.setFilled(true);
+		head.setFillColor(Color.GRAY);
+		add(head);
+	}
+	
+	/* Add an eye centered at (cx, cy) */
+	private void addEye (double cx, double cy) {
+		double x = cx - EYE_RADIUS;
+		double y = cy - EYE_RADIUS;
+		GOval eye = new GOval (x, y, 2 * EYE_RADIUS, 2 * EYE_RADIUS);
+		eye.setFilled(true);
+		eye.setColor(Color.YELLOW);
+		add(eye);
+	}
+	
+	/* Adds a mouth centered at (cx, cy) */
+	private void addMouth (double cx, double cy) {
+		double x = cx - MOUTH_WIDTH / 2;
+		double y = cy - MOUTH_HEIGHT / 2;
+		GOval mouth = new GOval (x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		mouth.setFilled(true);
+		mouth.setColor(Color.WHITE);
+		add(mouth);
+	}
 }
