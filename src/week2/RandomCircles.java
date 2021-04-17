0defe9123f22cad0da7df15bf99c5f7574ca9541
Penprapa Calhoun <mayyeewa@yahoo.com>
2021-04-17 06:45:51 -0400

    first.commit

--- src/week2/RandomCircles.java
+++ src/week2/RandomCircles.java
@@ -1,7 +1,35 @@
 package week2;
 
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class RandomCircles extends GraphicsProgram {
+public class RandomCircles<rgen, rgen1> extends GraphicsProgram {
+	
+	
+	/** Number of circles */
+		private static final int NCIRCLES = 10;
+		
+	/** Minimum radius */
+		private static final double MIN_RADIUS = 5;
+		
+	/** Maximum radius */
+		private static final double MAX_RADIUS = 50;
+		
+		public void run() {
+			for (int i = 0; i <NCIRCLES; i++) {
+				double r = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
+				double x = rgen.nextDouble(0, getWidth() - 2 * r);
+				double y = rgen.nextDouble(0, getHeight() - 2 * r);
+				GOval circle = new GOval (x, y, 2 * r, 2* r);
+				circle.setFilled(true);
+				circle.setColor(rgen.nextColor());
+				add(circle);
+			}
+			
+}
+	/* Private instance variable */
+		private RandomGenerator rgen = RandomGenerator.getInstance() ;
+
 
 }
