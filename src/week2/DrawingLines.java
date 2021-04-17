b1885548ff4dcd2714286d36194e6aed69dfe6ee
Penprapa Calhoun <mayyeewa@yahoo.com>
2021-04-17 07:15:48 -0400

    first.commit

--- src/week2/DrawingLines.java
+++ src/week2/DrawingLines.java
@@ -1,7 +1,32 @@
 package week2;
 
+import java.awt.event.MouseEvent;
+
+import javax.sound.sampled.Line;
+
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class DrawingLines extends GraphicsProgram {
-
+	public void run () {
+		addMouseListeners();
+	}
+	
+/** Called on mouse press to create a new line */
+	public void mousePressed(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		line = new GLine(x, y, x, y);
+		add(line);
 }
+	
+/** Called on mouse drag to reset the endpoint */
+	public void mouseDragged(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		line.setEndPoint(x,y);
+	}
+	
+	/* Private instance variables */
+		private GLine line;
+	}
